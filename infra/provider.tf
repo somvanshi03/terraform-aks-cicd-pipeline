fe4b931beb753896679e788d2d834385a376c971
Mridul Kumar Singh <msd8390@gmail.com>
2026-01-24 10:15:47 +0530

    Initial Commit

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,22 +1,7 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.100.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 4.0.5"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.5.1"
-    }
+required_providers {
+  azurerm = {
+    source  = "hashicorp/azurerm"
+    version = ">= 3.100.0"
   }
 }
 
-provider "azurerm" {
-  features {}
-}
